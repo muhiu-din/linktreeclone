620b55d25d79663ed2402f2af1a3d49c763a5de5
Muhiu Din <muhiudin005@gmail.com>
2025-08-25 02:20:18 +0500

    page initialized

--- linktreeclone/app/page.tsx
+++ linktreeclone/app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-green-600 min-h-screen pt-30">
-    <h1>Welcome to Home Page</h1>
+    <div className="bg-[#254f1a] min-h-[200vh] ">
+    <section className="grid grid-cols-2  items-center min-h-[100vh] pt-40">
+      <div className="bg-red-500 ml-10">
+        <div>
+          <p className="text-7xl font-bold">Everything you are. In one, simple link in bio.</p>
+        <p className="my-4">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <input className="bg-white text-black rounded-lg p-2 py-4" type="text" placeholder="linktr.ee/" />
+          <button className="bg-[#e9c0e9] p-2 py-4 rounded-full font-semibold text-black">Claim your linktree</button>
+        </div>
+      </div>
+      <div className="bg-slate-600 mr-10">This is col 2</div>
+    </section>
+    <section className="bg-blue-400 min-h-[100vh]">
+      
+    </section>
     </div>
   );
 }
--- linktreeclone/component/Navbar.tsx
+++ linktreeclone/component/Navbar.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar:React.FC = () => {
   return (
-    <nav className='bg-white text-black absolute flex flex-row justify-between top-8 w-[90vw] right-[5vw] p-4 rounded-full'>
+    <nav className='bg-white text-black  flex flex-row justify-between fixed top-8 w-[90vw] right-[5vw] p-3 rounded-full'>
       <div className='flex flex-row  items-center'>
-        <div className='mx-5'><img width={120}  src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg" alt="" /></div>
-      <ul className='flex flex-row gap-2'>
+        <div className='mx-8'><img width={120}  src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg" alt="" /></div>
+      <ul className='flex flex-row gap-6'>
         <li>Products</li>
         <li>Templates</li>
         <li>Marketplace</li>
